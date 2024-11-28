0b3b29cd251a0643c939b444ace7136612202844
niamatmarjan <mniamatmarjan@gmail.com>
2024-11-28 06:07:24 +0530

    feat: add searchbar and shadcn input

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,12 +1,14 @@
 import MobileNavMenu from "./mobile-nav-menu";
+import SearchBar from "./search-bar";
 import UserNav from "./user-nav";
 
 const Nav = () => {
   return (
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
-      <div className="mx-4 sm:mx-8 flex h-14 items-center">
+      <div className="mx-4 sm:mx-8 flex h-14 items-center gap-2">
         <div className="flex items-center space-x-4 lg:space-x-0">
           <MobileNavMenu />
+          <SearchBar />
         </div>
         <div className="flex flex-1 items-center justify-end">
           <UserNav />
