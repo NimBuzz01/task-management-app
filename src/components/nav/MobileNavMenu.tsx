75a8c32bc2d52e5eeb8e46e423e31dbdf8ac6615
niamatmarjan <mniamatmarjan@gmail.com>
2024-11-29 21:12:12 +0530

    feat: add logo component to sidebar and mobile nav menu

--- src/components/nav/MobileNavMenu.tsx
+++ src/components/nav/MobileNavMenu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { MenuIcon, PanelsTopLeft } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -9,6 +9,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import SidebarMenu from "../sidebar/SidebarMenu";
+import MainLogo from "../MainLogo";
 
 const MobileNavMenu = () => {
   return (
@@ -18,16 +19,16 @@ const MobileNavMenu = () => {
           <MenuIcon size={20} />
         </Button>
       </SheetTrigger>
-      <SheetContent className="sm:w-72 px-3 h-full flex flex-col" side="left">
+      <SheetContent className="sm:w-72 px-3 h-max flex flex-col" side="left">
         <SheetHeader>
+          <SheetTitle className="sr-only">Mobile Menu</SheetTitle>
           <Button
             className="flex justify-center items-center pb-2 pt-1"
             variant="link"
             asChild
           >
             <Link href="/" className="flex items-center gap-2">
-              <PanelsTopLeft className="w-6 h-6 mr-1" />
-              <SheetTitle className="font-bold text-lg">Logo</SheetTitle>
+              <MainLogo />
             </Link>
           </Button>
         </SheetHeader>
