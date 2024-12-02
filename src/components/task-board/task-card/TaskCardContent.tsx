9182df2f22dc6c9404ebb24a86838e5a3757d617
niamatmarjan <mniamatmarjan@gmail.com>
2024-12-03 01:44:00 +0530

    feat: update task card components (date,name) to fetch global states and conditions

--- src/components/task-board/task-card/TaskCardContent.tsx
+++ src/components/task-board/task-card/TaskCardContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import TaskCheck from "./TaskCardCheck";
 import TaskName from "./TaskCardName";
 import { Task } from "@/types/task";
@@ -11,49 +11,56 @@ import { useTaskSheet } from "@/store/useTaskSheet";
 import { isWithinWeekRange } from "@/lib/utils/task-utils";
 import TaskDateText from "./TaskCardDateText";
 import TaskDescription from "./TaskCardDescription";
+import { useTaskActions } from "@/hooks/useTaskActions";
+
+const isTaskReady = (task: Task) => {
+  return task.name && task.assignee && task.dueDate && task.priority;
+};
 
 const TaskCardContent = ({ task }: { task: Task }) => {
   const { setTask, setIsOpen } = useTaskSheet();
-  const [disabled, setDisabled] = useState(true);
-
-  const handleClick = () => {
-    setTask(task);
-    setIsOpen(true);
-  };
+  const { updateTaskProperty } = useTaskActions();
 
   useEffect(() => {
-    if (task.name && task.assignee && task.dueDate && task.priority) {
-      setDisabled(false);
+    if (task.isTemporary && isTaskReady(task)) {
+      updateTaskProperty(task.id, "isTemporary", false);
     }
-  }, [task]);
+  }, [task, updateTaskProperty]);
+
+  const handleClick = () => {
+    if (!task.isTemporary) {
+      setTask(task);
+      setIsOpen(true);
+    }
+  };
 
   return (
     <Card
-      className="bg-custom-generic-white"
-      onClick={disabled ? undefined : handleClick}
+      className="bg-custom-generic-white cursor-pointer"
+      onClick={handleClick}
     >
-      <div className="p-3 inline-flex w-full items-center">
-        <TaskCheck task={task} />
-        <TaskName task={task} disabled={!disabled} />
+      <div className="p-3 flex items-center w-full">
+        <TaskCheck status={task.status} />
+        <TaskName taskId={task.id} />
       </div>
       <Separator />
       {task.description && (
         <div className="p-2 px-4 overflow-hidden">
-          <TaskDescription task={task} />
+          <TaskDescription description={task.description} />
         </div>
       )}
       <div className="p-2 px-4 flex items-center gap-2">
-        <TaskAssignee task={task} disabled={!disabled} />
-        <TaskDate task={task} disabled={!disabled} />
+        <TaskAssignee taskId={task.id} />
+        <TaskDate taskId={task.id} />
         <div className="ml-auto">
-          <TaskPriority task={task} disabled={!disabled} />
+          <TaskPriority taskId={task.id} />
         </div>
       </div>
       {task.dueDate && isWithinWeekRange(task.dueDate) && (
-        <TaskDateText date={task.dueDate} />
+        <TaskDateText dueDate={task.dueDate} />
       )}
     </Card>
   );
 };
 
-export default TaskCardContent;
+export default React.memo(TaskCardContent);
--- src/components/task-board/task-card/TaskCardDate.tsx
+++ src/components/task-board/task-card/TaskCardDate.tsx
@@ -1,26 +1,26 @@
 import { DatePicker } from "@/components/ui/date-picker";
 import { useTaskActions } from "@/hooks/useTaskActions";
-import { Task } from "@/types/task";
-import React from "react";
 import { getDateStatusStyles } from "@/lib/utils/task-utils";
+import { useTaskStore } from "@/store/useTaskStore";
+import React from "react";
 
-const TaskCardDate = ({
-  task,
-  disabled,
-}: {
-  task: Task;
-  disabled?: boolean;
-}) => {
-  const { updateProperty } = useTaskActions();
+const TaskCardDate = ({ taskId }: { taskId: string }) => {
+  const { updateTaskProperty } = useTaskActions();
+
+  const task = useTaskStore((state) =>
+    state.tasks.find((task) => task.id === taskId)
+  );
+
+  if (!task) return null;
 
   return (
     <DatePicker
       value={task.dueDate}
-      onChange={(date) => updateProperty(task.id, "dueDate", date)}
+      onChange={(date) => updateTaskProperty(task.id, "dueDate", date)}
       className={task.dueDate ? getDateStatusStyles(task.dueDate) : ""}
-      disabled={disabled}
+      disabled={!task.isTemporary}
     />
   );
 };
 
-export default TaskCardDate;
+export default React.memo(TaskCardDate);
