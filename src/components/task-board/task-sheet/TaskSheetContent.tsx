0266ca5ecbc5e44689b9efad8a67171e83285e3c
niamatmarjan <mniamatmarjan@gmail.com>
2024-12-03 01:46:54 +0530

    revert: remove react-hook-form flow and use global state flow

--- src/components/task-board/task-sheet/TaskSheetContent.tsx
+++ src/components/task-board/task-sheet/TaskSheetContent.tsx
@@ -9,14 +9,7 @@ import TaskSheetAssignee from "./TaskSheetAssignee";
 import TaskSheetPriority from "./TaskSheetPriority";
 import TaskSheetDescription from "./TaskSheetDescription";
 import { useTaskSheet } from "@/store/useTaskSheet";
-import React, { useEffect } from "react";
-import { useForm, useWatch } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useTaskStore } from "@/store/useTaskStore";
-import { taskFormSchema } from "@/lib/utils/task-schema";
-import useDebounce from "@/hooks/useDebounce";
-import { Task } from "@/types/task";
-import { z } from "zod";
+import React from "react";
 import { Button } from "@/components/ui/button";
 
 const TaskSheetContent = () => {
@@ -26,34 +19,6 @@ const TaskSheetContent = () => {
     resetTask();
     setIsOpen(false);
   }, [resetTask, setIsOpen]);
-
-  const { updateTaskProperty } = useTaskStore();
-  const form = useForm<z.infer<typeof taskFormSchema>>({
-    resolver: zodResolver(taskFormSchema),
-    defaultValues: task || {},
-  });
-
-  const debouncedUpdate = useDebounce(
-    (id: string, property: keyof Task, value: any) => {
-      updateTaskProperty(id, property, value);
-    },
-    300
-  );
-
-  const name = useWatch({ control: form.control, name: "name" });
-  const description = useWatch({ control: form.control, name: "description" });
-
-  useEffect(() => {
-    if (name !== undefined && task) {
-      debouncedUpdate(task.id, "name", name);
-    }
-  }, [name]);
-
-  useEffect(() => {
-    if (description !== undefined && task) {
-      debouncedUpdate(task.id, "description", description);
-    }
-  }, [description]);
 
   if (!task) return null;
 
@@ -67,14 +32,14 @@ const TaskSheetContent = () => {
         </Button>
       </div>
       <div className="p-4 py-6 space-y-6">
-        <TaskSheetName {...form.register("name")} />
+        <TaskSheetName taskId={task.id} />
         <div className="space-y-6">
           <TaskSheetStatus taskId={task.id} />
           <TaskSheetDate taskId={task.id} />
           <TaskSheetAssignee taskId={task.id} />
           <TaskSheetPriority taskId={task.id} />
         </div>
-        <TaskSheetDescription {...form.register("description")} />
+        <TaskSheetDescription taskId={task.id} />
       </div>
     </div>
   );
